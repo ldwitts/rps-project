4f042492644ffd5c016df9cd25923ff276b2dec1
ldwitts <ldwitts@gmail.com>
2022-05-04 21:30:27 -0500

    Final version of console game

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 'use strict';
 
+// global variables
+let playerWins = 0;
+let playerLosses = 0;
+
+game();
+
+///////////// Functions /////////////
+// Get input from the user and make it uppercase
+function getPlayerSelection() {
+  let playerSelection = prompt('Please enter "rock", "paper", or "scissors"!');
+  playerSelection = playerSelection.toUpperCase();
+  return playerSelection;
+}
+
 // Generate and return a random move in the form of an uppercase string
-function computerPlay() {
+function getComputerSelection() {
   const randomNum = Math.floor(Math.random() * 3) + 1;
   let computerMove = '';
 
@@ -19,23 +33,17 @@ function computerPlay() {
   return computerMove.toUpperCase();
 }
 
-// Assign a random move to the computer
-const computerSelection = computerPlay();
-// console.log(computerSelection);
-
-// Get input from the user and make it uppercase
-let playerSelection = prompt('Please enter "rock", "paper", or "scissors"!');
-playerSelection = playerSelection.toUpperCase();
-
 // Main game logic, compare two selections and return the result
 function playRound(playerSelection, computerSelection) {
   let result = '';
-  let winMessage = `You picked ${playerSelection} and the computer picked\
- ${computerSelection}. Congratulations, you win!`;
-  let loseMessage = `You picked ${playerSelection} and the computer picked\
- ${computerSelection}. You lose, better luck next time.`;
 
-  // Result for a tie
+  // Messages to display if the player wins or loses
+  let winMessage = `You picked ${playerSelection} and the computer picked \
+${computerSelection}. Congratulations, you win!`;
+  let loseMessage = `You picked ${playerSelection} and the computer picked \
+${computerSelection}. You lose, better luck next time.`;
+
+  // Compare selections and determine appropriate result
   if (playerSelection === computerSelection) {
     result = `It's a tie! You both picked ${computerSelection.toLowerCase()}!`;
   } else if (playerSelection === 'ROCK') {
@@ -55,7 +63,27 @@ function playRound(playerSelection, computerSelection) {
       'Invalid entry. Next time please enter "rock", "paper", or' +
       ' "scissors"!';
   }
+
+  if (result === winMessage) playerWins++;
+  else if (result === loseMessage) playerLosses++;
+
   return result;
 }
 
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  let numberOfRounds = prompt(`How many rounds do you want to play?`);
+  numberOfRounds = Number(numberOfRounds);
+  for (let i = 0; i < numberOfRounds; i++) {
+    console.log(
+      playRound(getPlayerSelection(), getComputerSelection()),
+      `Player wins: ${playerWins} Computer wins: ${playerLosses}`
+    );
+  }
+  playerWins > playerLosses
+    ? console.log(
+        `You win the game! You had ${playerWins} wins and the computer had ${playerLosses}.`
+      )
+    : console.log(
+        `You lose the game! You had ${playerWins} wins and the computer had ${playerLosses}.`
+      );
+}
