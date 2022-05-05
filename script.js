2969a2edb7242bb769b1fa868d72cb9b1ae0a11f
ldwitts <ldwitts@gmail.com>
2022-05-04 20:26:24 -0500

    Finish playRound function
    Finished the logic for the playRound function which compares the
    computers choice and the players choice and returns a string declaring
    the winner.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-// 'use strict';
+'use strict';
 
+// Generate and return a random move in the form of an uppercase string
 function computerPlay() {
   const randomNum = Math.floor(Math.random() * 3) + 1;
   let computerMove = '';
@@ -18,15 +19,41 @@ function computerPlay() {
   return computerMove.toUpperCase();
 }
 
+// Assign a random move to the computer
 const computerSelection = computerPlay();
-console.log(computerSelection);
+// console.log(computerSelection);
+
+// Get input from the user and make it uppercase
 let playerSelection = prompt('Please enter "rock", "paper", or "scissors"!');
 playerSelection = playerSelection.toUpperCase();
 
+// Main game logic, compare two selections and return the result
 function playRound(playerSelection, computerSelection) {
   let result = '';
+  let winMessage = `You picked ${playerSelection} and the computer picked\
+ ${computerSelection}. Congratulations, you win!`;
+  let loseMessage = `You picked ${playerSelection} and the computer picked\
+ ${computerSelection}. You lose, better luck next time.`;
+
+  // Result for a tie
   if (playerSelection === computerSelection) {
     result = `It's a tie! You both picked ${computerSelection.toLowerCase()}!`;
+  } else if (playerSelection === 'ROCK') {
+    computerSelection === 'SCISSORS'
+      ? (result = winMessage)
+      : (result = loseMessage);
+  } else if (playerSelection === 'SCISSORS') {
+    computerSelection === 'PAPER'
+      ? (result = winMessage)
+      : (result = loseMessage);
+  } else if (playerSelection === 'PAPER') {
+    computerSelection === 'ROCK'
+      ? (result = winMessage)
+      : (result = loseMessage);
+  } else {
+    result =
+      'Invalid entry. Next time please enter "rock", "paper", or' +
+      ' "scissors"!';
   }
   return result;
 }
